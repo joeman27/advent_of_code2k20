80be106f0c7b9259a8fcd8b38363ed7e50dd4c9a
Jozef Mankovecky <jbm297@cornell.edu>
2020-12-07 15:05:32 -0500

    Day 5 pt. 2

--- day5/day5.py
+++ day5/day5.py
@@ -1,48 +1,45 @@
-from operator import methodcaller
+def get_pos(code):
+    upper = 127
+    lower = 0
+    for char in code[:7]:
+        if char == "F":
+            upper = (upper + lower) // 2
+        else:
+            lower = (upper + lower) // 2 + 1
+    row = lower
 
-class Plane():
-    def __init__(self):
-        
-    def find_empty_seats(self):
+    upper = 7
+    lower = 0
+    for char in code[7:]:
+        if char == "L":
+            upper = (upper + lower) // 2
+        else:
+            lower = (upper + lower) // 2 + 1
+    col = lower
 
-class Seat():
-    def __init__(self, code):
-        self.code = code
-        self.seat = self.getSeat()
-        self.ID = self.getID()
+    return (row, col)
 
-    def getSeat(self):
-        upper = 127
-        lower = 0
-        for char in self.code[:7]:
-            if char == "F":
-                upper = (upper + lower) // 2
-            else:
-                lower = (upper + lower) // 2 + 1
-        row = lower
+def get_ID(code):
+    (row, col) = get_pos(code)
+    return row*8 + col
 
-        upper = 7
-        lower = 0
-        for char in self.code[7:]:
-            if char == "L":
-                upper = (upper + lower) // 2
-            else:
-                lower = (upper + lower) // 2 + 1
-        
-        self.seat = (row, lower)
+def get_empties(codes):
+    seats = {(i, j): False for j in range(8) for i in range(128)}
+    for code in codes:
+        pos = get_pos(code)
+        seats[pos] = code
+    return seats
 
-    def getID(self):
-        (row, col) = self.seat
-        return row*8 + col
-
-    def __repr__(self):
-        return f"Code: {self.code}\nSeat: {self.seat}\nID: {self.ID}\n\n"
 
 def part1():
     with open("../files/day5", "r") as f:
-        seats = [Seat(line.strip()) for line in f.readlines()]
-        seats = sorted(seats, key=methodcaller("getID"))
-        print(seats)
+        print(f"Max: {max([get_ID(code.strip()) for code in f.readlines()])}")
 
+def part2():
+    with open("../files/day5", "r") as f:
+        with open("day_5_output", "w") as w:
+            seats = get_empties([code.strip() for code in f.readlines()])
+            print(seats)
 
 part1()
+part2()
